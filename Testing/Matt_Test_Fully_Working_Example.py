248b1336a2eb818d3d9ff02b9127e07079e9ff6b
Matt <matto87600@gmail.com>
2018-09-09 19:33:26 +1000

    .

--- Testing/Matt_Test_Fully_Working_Example.py
+++ Testing/Matt_Test_Fully_Working_Example.py
@@ -1,3 +1,4 @@
+# To run this code in atom you will have to download the platformio-ide-terminal package, go to the working directory and execute the code via py (filename here)
 import random
 namenum = int(input("how many characters?")) #taking numeric input
 while namenum > 0: #runs as many times as the input due to line 5
