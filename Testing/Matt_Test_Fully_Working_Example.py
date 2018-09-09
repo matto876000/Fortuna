62c5abb4a7ce4f7f938411cc5dcd4784485abece
Matt <matto87600@gmail.com>
2018-09-09 19:24:45 +1000

    .

--- Testing/Matt_Test_Fully_Working_Example.py
+++ Testing/Matt_Test_Fully_Working_Example.py
@@ -1,6 +1,5 @@
 import random
-namenum = int(input("how many characters?"))
-while namenum > 0:
-    print(random.choice(list(open('Matt_Test_Names.txt'))))
+namenum = int(input("how many characters?")) #taking numeric input
+while namenum > 0: #runs as many times as the input due to line 5
+    print(random.choice(list(open('Matt_Test_Names.txt')))) #printing a random name
     namenum = namenum -1
-    
