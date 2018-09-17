f4f12080610b23f15c4db197b7a537a27b859a29
Matt <matto87600@gmail.com>
2018-09-17 15:47:39 +1000

    Working example of names

---
+++ Testing/Matt_Test_Fully_Working_Example.py
@@ -0,0 +1,6 @@
+# To run this code in atom you will have to download the platformio-ide-terminal package, go to the working directory and execute the code via py (filename here)
+import random
+namenum = int(input("how many characters?")) #taking numeric input
+while namenum > 0: #runs as many times as the input due to line 5
+    print(random.choice(list(open('Human Male Names.txt')))) #printing a random name
+    namenum = namenum -1
