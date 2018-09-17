467342360ec3344810061d6aef83b288db98f725
Matt <matto87600@gmail.com>
2018-09-17 16:11:02 +1000

    Updating syntax and ordering of code

--- Testing/Name Generator.py
+++
@@ -1,7 +0,0 @@
-import random
-loop = 0 
-while loop < 1: #Number is how many names you want
-    f = open('Human Male Names.txt') #Open name file
-    lines = list(f) #Write each line of the file into a list
-    print (random.choice(lines)) #Print a random element from list
-    loop +=1
