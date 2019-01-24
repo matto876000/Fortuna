8e49049e015711f1d55e76bf401104977a17a4a3
Matt <matto87600@gmail.com>
2019-01-24 22:00:37 +1000

    update

---
+++ Character Generation/Character Generator.py
@@ -0,0 +1,4 @@
+import random
+Race = ['Aarakocra', 'Aasimar', 'Beholder', 'Changeling', 'Deep Gnome', 'Deva', 'Dragonborn', 'Drow', 'Dwarf', 'Eladrin', 'Elf', 'Firbolg', 'Genasi', 'Githzerai', 'Gnoll', 'Gnome', 'Goliath', 'Hag', 'Half-Elf', 'Half-Orc', 'Halfling', 'Human', 'Kenku', 'Kobold', 'Lizardfolk', 'Mind Flayer', 'Minotaur', 'Orc', 'Shardmind', 'Shifter', 'Tabaxi', 'Tiefling', 'Triton', 'Warforged', 'Wilden', 'Yuan-ti']
+Gender = ['Male', 'Female']
+Character_Race = (random.choice(Race))
