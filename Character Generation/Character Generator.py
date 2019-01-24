d9d2fca7adb441dd71192646127c13da168874ba
Matt <matto87600@gmail.com>
2019-01-24 23:09:30 +1000

    Major Update: -work on race descrptions -Work on character generation -unecessary races removed

--- Character Generation/Character Generator.py
+++ Character Generation/Character Generator.py
@@ -1,4 +1,16 @@
 import random
-Race = ['Aarakocra', 'Aasimar', 'Beholder', 'Changeling', 'Deep Gnome', 'Deva', 'Dragonborn', 'Drow', 'Dwarf', 'Eladrin', 'Elf', 'Firbolg', 'Genasi', 'Githzerai', 'Gnoll', 'Gnome', 'Goliath', 'Hag', 'Half-Elf', 'Half-Orc', 'Halfling', 'Human', 'Kenku', 'Kobold', 'Lizardfolk', 'Mind Flayer', 'Minotaur', 'Orc', 'Shardmind', 'Shifter', 'Tabaxi', 'Tiefling', 'Triton', 'Warforged', 'Wilden', 'Yuan-ti']
+import math
+Race = ['Aarakocra', 'Dragonborn', 'Drow', 'Dwarf', 'Elf', 'Gnoll', 'Gnome', 'Half-Elf', 'Half-Orc', 'Halfling', 'Human', 'Kenku', 'Kobold', 'Lizardfolk', 'Mind Flayer', 'Minotaur', 'Orc', 'Tiefling', 'Yuan-ti']
+Race = ['Human']
 Gender = ['Male', 'Female']
 Character_Race = (random.choice(Race))
+print (Character_Race)
+if Character_Race == "Human":
+    fHuman = open('Names/Human Male Names.txt')
+    HumanLines = list(fHuman)
+    print (random.choice(HumanLines))
+    RawAge = random.betavariate(2.25,4.2)
+    RawAge = (RawAge*100)
+    Age = round (RawAge)
+    print (Age)
+    
